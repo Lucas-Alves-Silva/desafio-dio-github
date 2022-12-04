28e42287d05bd38a6681265b005064857ed80838
Lucas Alves <65175946+Lucas-Alves-Silva@users.noreply.github.com>
2022-12-03 22:26:16 -0300

    Desafio Github DIO

---
+++ desafio02-github-wiki/src/pages/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import logotipo from '../assets/logotipo.png'
+import Input from '../components/Input';
+import Button from '../components/Button';
+import ItemRepo from '../components/ItemRepo';
+import { api } from '../services/api';
+
+import { Container } from './styles';
+
+function App() {
+
+  const [currentRepo, setCurrentRepo] = useState('');
+  const [repos, setRepos] = useState([]);
+
+const handleSearchRepo = async () => {
+
+  const {data} = await api.get(`repos/${currentRepo}`)
+
+  if(data.id){
+
+    const isExist = repos.find(repo => repo.id === data.id)
+
+      if(!isExist){
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('')
+        return
+      }
+    }
+    alert('Repositório já foi encontrado.')
+    setCurrentRepo('')
+}
+
+const handleRemoveRepo = (id) => {
+
+}
+
+
+  return (
+    <Container>
+      <img src={logotipo} width={72} height={72} alt="git hub logo"/>
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)}/>
+      <Button onClick={handleSearchRepo}/>
+      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+    </Container>
+  );
+}
+
+export default App;
